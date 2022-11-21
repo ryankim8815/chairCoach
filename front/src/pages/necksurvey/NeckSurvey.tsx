9ef98d92a6eb1e47230884eb92277a0b7fd991d2
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-11-21 14:44:40 +0900

    [Feature]: router

---
+++ front/src/pages/necksurvey/NeckSurvey.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NeckSurvey = () => {
+  return (
+    <div>
+      목 서베이
+    </div>
+  )
+}
+
+export default NeckSurvey
---
+++ front/src/pages/necksurvey/NeckSurveyStyle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NeckSurveyStyle = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NeckSurveyStyle
