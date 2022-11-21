d81ebf0bb5f72b5ad17131df862bd6c2ce41dbbe
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-11-21 20:12:36 +0900

    [Feature]: icon by score

--- front/src/pages/surveyresult/SurveyResult.tsx
+++ front/src/pages/surveyresult/SurveyResult.tsx
@@ -1,16 +1,56 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  HalfBox,
+  IconImg,
+  Percent,
+  RecommendBox,
+  ResultBox,
+  ResultContainer,
+  SubTitle,
+  TextBox,
+  Title,
+  TitleBox,
+} from "./SurveyResultStyle";
+import result1 from '../../assets/img/result1.png'
+import result2 from '../../assets/img/result2.png'
+import result3 from '../../assets/img/result3.png'
 
 const SurveyResult = () => {
-    const location=useLocation();
-    const point=location.state.point
-
-    console.log('여기서나와야함',point)
+  const location = useLocation();
+  const point:number = location.state.point;
+  
+  
   return (
-    <div>
-      result
-    </div>
-  )
-}
+    <ResultContainer>
+      <TitleBox>
+        <Title>자가진단 결과</Title>
+      </TitleBox>
+      <ResultBox>
+        <HalfBox>
+          <TextBox>
+            <SubTitle>
+              설문 조항 결과에 따르면, name님의 거북목 위험도는 <Percent>{point * 10}%</Percent>
+              입니다!
+            </SubTitle>
+          </TextBox>
+        </HalfBox>
+        <HalfBox style={{marginRight:129}}>
+            {
+                point>=7
+                ? <IconImg src={result1}/>
+                : ( point>=4
+                    ? <IconImg src={result2}/>
+                    : <IconImg src={result3}/>)
+            }
+            
+        </HalfBox>
+      </ResultBox>
+      <RecommendBox>
+        
+      </RecommendBox>
+    </ResultContainer>
+  );
+};
 
-export default SurveyResult
+export default SurveyResult;
--- front/src/pages/surveyresult/SurveyResultStyle.tsx
+++ front/src/pages/surveyresult/SurveyResultStyle.tsx
@@ -1,1 +1,76 @@
 import styled from "styled-components";
+
+
+export const ResultContainer=styled.div`
+height: 100vh;
+width: 100%;
+display:flex;
+justify-content:center;
+align-items:center;
+background: ${({theme})=>theme.colors.mainLight};
+flex-direction:column;
+`
+
+
+export const Title=styled.span`
+font-size:${({theme})=>theme.fontSize.title}px;
+`
+export const SubTitle=styled.span`
+font-size:${({theme})=>theme.fontSize.subTitle}px;
+`
+
+
+export const TitleBox=styled.div`
+width:800px;
+height: auto;
+display:flex;
+align-items: center;
+margin-bottom: 10px;
+`
+
+
+export const ResultBox=styled.div`
+width:800px;
+height:280px;
+background-color:#FFFFFF;
+display:flex;
+margin-top:20px;
+`
+
+
+export const RecommendBox=styled.div`
+width:600px;
+height:270px;
+display:flex;
+flex-direction:column;
+background-color:#FFFFFF;
+margin-top:40px;
+aling-items:center;
+`
+
+
+export const BtnBox=styled.div`
+display:flex;
+justify-content:center;
+
+`
+export const HalfBox=styled.div`
+width:400px;
+height:280px;
+display:flex;
+flex-direction:row-reverse;
+align-items:center;
+`
+export const TextBox=styled.div`
+width:262px;
+height:105px;
+`
+export const Percent=styled.span`
+color:${({theme})=>theme.colors.main};
+font-weight:600;
+`
+
+export const IconImg=styled.img`
+width:160px;
+height:160px;
+`
