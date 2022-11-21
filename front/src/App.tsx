2015cf5a930490dda7b2534c792c37e510b2f16a
zzzzsb <ji12subin@gmail.com>
2022-11-21 20:18:12 +0900

    feat: main page layout

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,27 +1,26 @@
-import GlobalStyles from './styles/GlobalStyle';
+import GlobalStyles from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import theme from './styles/Theme';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainPage from './pages/mainpage/MainPage';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import NeckSurvey from './pages/necksurvey/NeckSurvey';
+import theme from "./styles/Theme";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/mainpage/Main";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import NeckSurvey from "./pages/necksurvey/NeckSurvey";
+
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <GlobalStyles />
-      <ThemeProvider theme={theme}>
-        <Routes>
-          <Route path='/' element={<MainPage/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/necksurvey' element={<NeckSurvey/>}/>
-          <Route path='/' element={<MainPage/>}/>
-          <Route path='/' element={<MainPage/>}/>
-
-        </Routes>
-      </ThemeProvider>
+      <BrowserRouter>
+        <GlobalStyles />
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/necksurvey" element={<NeckSurvey />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </>
   );
--- front/src/styles/Theme.ts
+++ front/src/styles/Theme.ts
@@ -2,18 +2,18 @@ import { DefaultTheme } from "styled-components";
 
 const colors = {
   // mainColor
-  main: '#835DFE',
-  mainLight: '#F1EDFC', // 페이지bg
-  mainMoreLight: '#FAF9FF', // 컨포넌트 밝은 보라 bg
-  mainDark: '#403E56', // footer, 어두운버튼
+  main: "#835DFE",
+  mainLight: "#F1EDFC", // 페이지bg
+  mainMoreLight: "#FAF9FF", // 컨포넌트 밝은 보라 bg
+  mainDark: "#403E56", // footer, 어두운버튼
 
   // grey
-  greyText: '#999', // 회색폰트
-  greyBorder: '#e1e1e1', // 회색보더
-  greyBtnBg: '#F6F6F7', // 회색버튼배경
+  greyText: "#999", // 회색폰트
+  greyBorder: "#e1e1e1", // 회색보더
+  greyBtnBg: "#F6F6F7", // 회색버튼배경
 
   // the rest
-  warning: '#E57683', // input경고
+  warning: "#E57683", // input경고
 };
 
 const fontSize = {
