564dc7f3e3c8bc8e317f2130310a3211805da5f3
zzzzsb <ji12subin@gmail.com>
2022-11-23 22:40:06 +0900

    Feat: complete main page

--- front/src/pages/main/Main.tsx
+++ front/src/pages/main/Main.tsx
@@ -3,12 +3,16 @@ import * as S from "./MainStyle";
 import * as B from "../../styles/BtnStyle";
 import Banner from "./Banner";
 import IntroduceLayout from "./IntroduceLayout";
+import ExplainLayout from "./ExplainLayout";
+import SelectLayout from "./SelectLayout";
 
 const Main = () => {
   return (
     <S.MainLayout>
       <Banner />
       <IntroduceLayout />
+      <ExplainLayout />
+      <SelectLayout />
     </S.MainLayout>
   );
 };
