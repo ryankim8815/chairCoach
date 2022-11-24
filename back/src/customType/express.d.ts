9b41e05ee2cd0c04932b1c9f0b52dcade5b6bb49
david <p33a33@gmail.com>
2022-11-24 19:36:38 +0900

    cache clean

--- back/src/customType/express.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace Express {
-  interface Request {
-    user_id?: string;
-    filename?: string;
-    // files?: any; // something like `multer.Files`
-  }
-}
--- back/src/customType/multer.d.ts
+++
@@ -1,7 +0,0 @@
-export interface MulterFile {
-  key: string; // Available using `S3`.
-  path: string; // Available using `DiskStorage`.
-  mimetype: string;
-  originalname: string;
-  size: number;
-}
