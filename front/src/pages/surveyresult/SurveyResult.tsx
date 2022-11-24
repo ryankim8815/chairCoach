4c73d75f960c9c100fd3764c834a472f64cca2ff
유민지 <yyoumi4854@gmail.com>
2022-11-24 14:51:45 +0900

    [Fix] singup

--- front/src/pages/surveyresult/SurveyResult.tsx
+++ front/src/pages/surveyresult/SurveyResult.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import * as S from "./SurveyResultStyle";
 import result1 from "../../assets/img/result1.png";
