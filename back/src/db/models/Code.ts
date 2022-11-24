f445177cc4ab34e169385927fd425aabf29da633
Kim Byeongyong <ryankim8815@gmail.com>
2022-11-25 00:18:16 +0900

    [Feat]: Code.ts - added expiration period for email verification code

--- back/src/db/models/Code.ts
+++ back/src/db/models/Code.ts
@@ -3,7 +3,7 @@ class Code {
   static async create({ email, code }) {
     const [rows, fields] = await promisePool.query({
       //   sql: "INSERT INTO codes (email, code) VALUES (?, ?)",
-      sql: "REPLACE INTO codes (email, code) VALUES (?, ?)",
+      sql: "REPLACE INTO codes (email, code, created_at) VALUES (?, ?, NOW())",
       values: [email, code],
     });
     return rows;
