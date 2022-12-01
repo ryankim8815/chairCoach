84c9876d2e6feb90872bc6f824296290076c17b2
Kim Byeongyong <ryankim8815@gmail.com>
2022-12-01 22:17:06 +0900

    [Style]: bodyController.ts - separated Controller from Router

--- back/src/routers/bodyRouter.ts
+++ back/src/routers/bodyRouter.ts
@@ -1,28 +1,31 @@
 import * as express from "express";
-import bodyService from "../services/bodyService";
+import bodyController from "../controllers/bodyController";
 import authMiddleware from "../middlewares/authMiddleware";
 import * as validation from "../middlewares/bodyValidationMiddleware";
-import type { MulterFile } from "../customType/multer.d";
-const bodyRouter = express.Router();
 
-// GET: 전체 운동 기록 조회 기능
-const bodyRecordlist = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  try {
-    const allBodies = await bodyService.getAllBodies();
-    return res.status(200).json(allBodies);
-  } catch (err) {
-    const result_err = {
-      result: false,
-      cause: "api",
-      message: "bodyRecordlist api에서 오류가 발생했습니다.",
-    };
-    return res.status(200).json(result_err);
-  }
-};
+const bodyRouter = express.Router();
+bodyRouter.get("/bodies", bodyController.bodyRecordlist); // 전체 운동 기록 조회 기능, 개발시 편의를 위한 기능으로 사용처가 없다면 삭제 예정
+bodyRouter.get(
+  "/body",
+  authMiddleware,
+  validation.validateBodyRecords,
+  bodyController.bodyRecords
+); // 특정 유저의 운동 기록 조회
+bodyRouter.post(
+  "/body",
+  authMiddleware,
+  validation.validateBodyCreate,
+  bodyController.bodyCreate
+); // 특정 유저의 운동 기록 시작
+bodyRouter.patch(
+  "/body",
+  authMiddleware,
+  validation.validateBodyUpdate,
+  bodyController.bodyUpdate
+); // 특정 유저의 운동 기록 종료
+
+export = bodyRouter;
+
 /**
  * @swagger
  * /bodies:
@@ -72,25 +75,6 @@ const bodyRecordlist = async (
  *                       end_time: 2022-11-01T01:01:01.000Z
  */
 
-// GET: 특정 유저의 운동 기록 조회
-const bodyRecords = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  try {
-    const user_id = req.body.user_id;
-    const Bodies = await bodyService.getBodies({ user_id });
-    return res.status(200).json(Bodies);
-  } catch (err) {
-    const result_err = {
-      result: false,
-      cause: "api",
-      message: "bodyRecords api에서 오류가 발생했습니다.",
-    };
-    return res.status(200).json(result_err);
-  }
-};
 /**
  * @swagger
  * /body:
@@ -142,29 +126,6 @@ const bodyRecords = async (
  *                       end_time: 2022-11-01T01:01:01.000Z
  */
 
-// POST: 특정 유저의 운동 기록 시작
-const bodyCreate = async (
-  req: express.Request & { files: MulterFile[] },
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  try {
-    const user_id = req.body.user_id;
-    const tag = req.body.tag;
-    const body = await bodyService.addBody({
-      user_id,
-      tag,
-    });
-    return res.status(200).json(body);
-  } catch (err) {
-    const result_err = {
-      result: false,
-      cause: "api",
-      message: "bodyCreate api에서 오류가 발생했습니다.",
-    };
-    return res.status(200).json(result_err);
-  }
-};
 /**
  * @swagger
  * /body:
@@ -205,29 +166,6 @@ const bodyCreate = async (
  *                   example: fawa524tweryht3w
  */
 
-// PATCH: 특정 유저의 운동 기록 종료
-const bodyUpdate = async (
-  // req: express.Request & { files: MulterFile[] },
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  try {
-    const user_id = req.body.user_id; // 확인하는 것으로 수정 예정
-    const body_id = req.body.body_id;
-    const body = await bodyService.updateBody({
-      body_id,
-    });
-    return res.status(200).json(body);
-  } catch (err) {
-    const result_err = {
-      result: false,
-      cause: "api",
-      message: "bodyUpdate api에서 오류가 발생했습니다.",
-    };
-    return res.status(200).json(result_err);
-  }
-};
 /**
  * @swagger
  * /body:
@@ -264,25 +202,3 @@ const bodyUpdate = async (
  *                   type: string
  *                   example: 해당 유저의 운동 기록 종료가 성공적으로 이뤄졌습니다.
  */
-
-bodyRouter.get("/bodies", bodyRecordlist); // 전체 운동 기록 조회 기능, 개발시 편의를 위한 기능으로 사용처가 없다면 삭제 예정
-bodyRouter.get(
-  "/body",
-  authMiddleware,
-  validation.validateBodyRecords,
-  bodyRecords
-); // 특정 유저의 운동 기록 조회
-bodyRouter.post(
-  "/body",
-  authMiddleware,
-  validation.validateBodyCreate,
-  bodyCreate
-); // 특정 유저의 운동 기록 시작
-bodyRouter.patch(
-  "/body",
-  authMiddleware,
-  validation.validateBodyUpdate,
-  bodyUpdate
-); // 특정 유저의 운동 기록 종료
-
-export = bodyRouter;
