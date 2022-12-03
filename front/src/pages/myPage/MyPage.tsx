9d038b26853c47fa174cdf0f3004d3409d3d533e
zzzzsb <ji12subin@gmail.com>
2022-12-04 00:15:41 +0900

    Feat: get body data

--- front/src/pages/myPage/MyPage.tsx
+++ front/src/pages/myPage/MyPage.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import { useRecoilState } from "recoil";
+import userState from "../../atoms/user";
 import * as S from "./MyPageStyle";
 import * as Api from "../../api/api";
 import MyChairReport from "../../components/myChairReport/MyChairReport";
 import TurtleNeckResult from "../../components/turtleNeckResult/TurtleNeckResult";
 
 const MyPage = () => {
-  const [turtleNeckResult, setTurtleNeckResult] = useState([]);
+  const [user, setUser] = useRecoilState(userState);
 
-  // try{
-  //   const res = Api.get("necks");
-  // }
+  const [myChairReport, setMyChairReport] = useState([]);
+  const [turtleNeckResult, setTurtleNeckResult] = useState([]);
+  useEffect(() => {
+    try {
+      Api.get("body").then((res) => setMyChairReport(res.data));
+      console.log(myChairReport);
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
+
+  console.log(turtleNeckResult);
   return (
     <S.MyPageLayout>
       <div className="inner">
