2b8a188a035c7b165600f121f59fec08e9b51c85
minglet <mg980504@gmail.com>
2022-12-05 18:45:59 +0900

    [Feat] run.py : FastAPI socket model prediction

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
 from xgboost import XGBClassifier
 import numpy as np
-import pandas as pd
 
 
 app = FastAPI(title="ChairCouchModel", description="API for Pose classification", version="1.0")
@@ -20,9 +19,12 @@ actions = np.array(['hands_up', 'neck_down', 'neck_side'])
 async def root():
     return {"message": "Hello World"}
 
+
+    
 @app.post('/predict', tags=["predictions"])
 async def get_prediction(coords:xyCoods):
     df = list(coords.dict().values())
     pred = model.predict(df)
     action = actions[np.argmax(pred)]
     return {"prediction": action}
+
