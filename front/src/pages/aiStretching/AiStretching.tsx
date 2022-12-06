22abbe5df2139a9109f5e2b0dcd97e45f66c0ea7
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-12-06 13:37:00 +0900

    [Feat]: socket(test), fix dataform

--- front/src/pages/aiStretching/AiStretching.tsx
+++ front/src/pages/aiStretching/AiStretching.tsx
@@ -1,8 +1,9 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
 import { drawKeypoints, drawSkeleton } from "./util";
 import * as poseDetection from "@tensorflow-models/pose-detection";
+import { Socket, io } from "socket.io-client";
 
 require("@tensorflow/tfjs");
 
@@ -10,6 +11,9 @@ const AiStretching = () => {
   const deviceId = useState("");
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  
+  const socketUrl=process.env.REACT_APP_SOCKET_URL
+  const socket= io(socketUrl as string)
   //user media device select할 때 일단 콘솔.. 이걸 적용시켜야함
   navigator.mediaDevices
     .enumerateDevices()
@@ -28,7 +32,19 @@ const AiStretching = () => {
       (webcamRef.current as any).video.width = videoWidth;
       (webcamRef.current as any).video.height = videoHeight;
       const pose = await detector.estimatePoses(video, {});
-      console.log("좌표값", pose[0].keypoints.slice(0, 11));
+      let dataArr:any=[];
+      const dataToSend=pose[0].keypoints.slice(0, 11)
+      // console.log("좌표값", dataToSend);
+      // console.log(JSON.stringify(dataToSend))
+      // dataToSend.filter((item)=>console.log([item.x,item.y]))
+      if(dataToSend){dataToSend.forEach((item)=>{
+        dataArr.push(item.x);
+        dataArr.push(item.y);
+        dataArr.push(item.score)
+      });
+    }
+      console.log( dataArr)
+      socket.emit('stretching',dataArr)
       drawResult(pose, video, videoWidth, videoHeight, canvasRef);
 
       requestAnimationFrame(() => {
