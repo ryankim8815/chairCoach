af3f5cd709c008091e126e16c1bdef24fcac6611
minglet <mg980504@gmail.com>
2022-12-07 01:50:58 +0900

    [Feat]  run.py - Only connection

--- fastapi/run.py
+++ fastapi/run.py
@@ -8,72 +8,37 @@ import socketio
 import numpy as np
 from xgboost import XGBClassifier
 
-# socket_manager = SocketManager(app=app)
-sio: Any = socketio.AsyncServer(async_mode="asgi")
+CLIENT_URLS = ["http://localhost:3000", "ws://localhost:3000"]
+
+sio: Any = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=CLIENT_URLS)
 socket_app = socketio.ASGIApp(sio)
 
 app = FastAPI()
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
 
 model = XGBClassifier()
 model.load_model('first_model.json')
 
 actions = np.array(['hands_up', 'neck_down', 'neck_side'])
 
-
-
-@app.get("/test")
-async def get():
-    return "Hello World"
-
-
 app.mount("/", socket_app)
-@sio.on("connect")
-async def connect():
-    print("Connected")
+
+@sio.event
+async def connect(sid, env, auth):
+    print(f"Connected to frontend with socket ID: {sid}")
+    await sio.emit("message", f"Backend has connected to using socket ID: {sid}")
     
 @sio.on("model")
-async def model(data):
+async def model(_, data):
     df = list(data.values())
     pred = model.predict(df)
     action = actions[np.argmax(pred)]
-    await sio.emit("model", action)
+    await sio.emit("model", f"{action}")
 
+@sio.event
+def disconnect(sid):
+    print(f"Socket with ID {sid} has disconnected")
 
 if __name__ == "__main__":
-    kwargs = {"host": "0.0.0.0", "port": 8000}
+    kwargs = {"port": 8000}
     kwargs.update({"reload":True})
     uvicorn.run("run:app", **kwargs)
