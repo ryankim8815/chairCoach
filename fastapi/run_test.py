e0a1e6c27ac3baeb714528008e41b03c7fe03c54
minglet <mg980504@gmail.com>
2022-12-09 21:46:16 +0900

    [Test] test code

--- fastapi/run_test.py
+++ fastapi/run_test.py
@@ -4,21 +4,39 @@ import uvicorn
 import socketio
 import numpy as np
 from load_model import ChairCouchModel
+from fastapi.middleware.cors import CORSMiddleware
 
-CLIENT_URLS = ["http://localhost:3000", "ws://localhost:3000"]
+CLIENT_URLS = ["http://localhost", "ws://localhost","ws://34.64.95.214","https://kdt-ai5-team04-gpu.elicecoding.com:443", "ws://221.156.20.146","http://localhost:3000", "ws://localhost:3000","http://localhost:3000/aistretching", "https://kdt-ai5-team04-gpu.elicecoding.com/aistretching","http://kdt-ai5-team04-gpu.elicecoding.com:3000/aistretching"]
+
+CLIENT_URLS = ["*"]
 
 sio: Any = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=CLIENT_URLS)
 socket_app = socketio.ASGIApp(sio)
 
 app = FastAPI()
 
+origins = ["*"]
+
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        )
+
 model = ChairCouchModel()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 
 @app.get("/test")
 async def test():
     print("test")
     return "Works"
-    
 
 app.mount("/", socket_app)
 
@@ -26,7 +44,7 @@ app.mount("/", socket_app)
 async def connect(sid, env, auth):
     print(f"Connected to frontend with socket ID: {sid}")
     await sio.emit("message", f"Backend has connected to using socket ID: {sid}")
-    
+
 @sio.on("model")
 async def model_predict(_, data):
     df = list(data.values())
@@ -35,9 +53,9 @@ async def model_predict(_, data):
 
 @sio.event
 def disconnect(sid):
-    print(f"Socket with ID {sid} has disconnected")
+    print("Socket with ID {sid} has disconnected")
 
 if __name__ == "__main__":
-    kwargs = {"port": 8000}
+    kwargs = {"host":"0.0.0.0","port": 5005}
     kwargs.update({"reload":True})
     uvicorn.run("run:app", **kwargs)
