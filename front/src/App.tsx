f8d49cdd6d96223f13c8889c1050b31ce9c48cd2
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-12-11 02:42:40 +0900

    [Feat]: aistrecthing page setting use params and dummy data

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -24,6 +24,7 @@ import NaverLogin from "./components/naverLogin/Naver";
 import MyPage from "./pages/myPage/MyPage";
 import GoogleLogin from "./components/googleLogin/GoogleLogin";
 import AiStretching from "./pages/aiStretching/AiStretching";
+import StretchingGuide from "./pages/stretchingGuide/StretchingGuide";
 
 function App() {
   return (
@@ -49,7 +50,8 @@ function App() {
             <Route path="login/oauth2/code/kakao" element={<KakaoAuth />} />
             <Route path="login/oauth2/code/naver" element={<NaverLogin />} />
             <Route path="login/oauth2/code/google" element={<GoogleLogin />} />
-            <Route path="/aistretching" element={<AiStretching />} />
+            <Route path="/aistretching/:id" element={<AiStretching />} />
+            <Route path="/stretchingguide/:id" element={<StretchingGuide />} />
           </Routes>
           <Footer />
         </ThemeProvider>
