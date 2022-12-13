5f1bdaa8a2e6e8bc29591af794d4686f2ab67904
Kim Byeongyong <ryankim8815@gmail.com>
2022-12-13 13:43:33 +0900

    [Feat]: index.ts - added HTTPS for api(test)

--- back/index.js
+++ back/index.js
@@ -1,40 +1,38 @@
 "use strict";
-// // //////////
-// import app from "./src/app";
-// import "dotenv/config";
-// import fs from "fs";
-// import HTTPS from "https";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// require("dotenv").config();
-// const PORT: string = process.env.SERVER_PORT;
-// const myUrl = "kdt-ai5-team04.elicecoding.com";
-// // const app = express();
-// console.log(
-//   "==========================URL",
-//   `/etc/letsencrypt/live/${myUrl}/fullchain.pem`
-// );
-// console.log("BEFORE OPTION");
-// const option = {
-//   ca: fs.readFileSync(`/etc/letsencrypt/live/${myUrl}/fullchain.pem`),
-//   key: fs.readFileSync(`/etc/letsencrypt/live/${myUrl}/privkey.pem`),
-//   cert: fs.readFileSync(`/etc/letsencrypt/live/${myUrl}/cert.pem`),
-// };
-// console.log("BEFORE HTTPS.create....");
-// HTTPS.createServer(option, app)
-//   .listen(PORT, () => {
-//     console.log("HTTPS 서버가 실행되었습니다. 포트 :: " + PORT);
-//   })
-//   .on("error", (err: string) => console.log(err));
-// // //////////
+// //////////
 var app_1 = __importDefault(require("./src/app"));
 require("dotenv/config");
+var fs_1 = __importDefault(require("fs"));
+var https_1 = __importDefault(require("https"));
 require("dotenv").config();
 var PORT = process.env.SERVER_PORT;
-app_1.default
+var myUrl = "kdt-ai5-team04.elicecoding.com";
+// const app = express();
+console.log("==========================URL", "/etc/letsencrypt/live/".concat(myUrl, "/fullchain.pem"));
+console.log("BEFORE OPTION");
+var option = {
+    ca: fs_1.default.readFileSync("/etc/letsencrypt/live/".concat(myUrl, "/fullchain.pem")),
+    key: fs_1.default.readFileSync("/etc/letsencrypt/live/".concat(myUrl, "/privkey.pem")),
+    cert: fs_1.default.readFileSync("/etc/letsencrypt/live/".concat(myUrl, "/cert.pem")),
+};
+console.log("BEFORE HTTPS.create....");
+https_1.default.createServer(option, app_1.default)
     .listen(PORT, function () {
-    console.log("\uC815\uC0C1\uC801\uC73C\uB85C \uC11C\uBC84\uB97C \uC2DC\uC791\uD558\uC600\uC2B5\uB2C8\uB2E4.  http://localhost:".concat(PORT));
+    console.log("HTTPS 서버가 실행되었습니다. 포트 :: " + PORT);
 })
     .on("error", function (err) { return console.log(err); });
+// //////////
+// ///// 기존 http /////
+// import app from "./src/app";
+// import "dotenv/config";
+// require("dotenv").config();
+// const PORT: string = process.env.SERVER_PORT;
+// app
+//   .listen(PORT, () => {
+//     console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+//   })
+//   .on("error", (err: string) => console.log(err));
