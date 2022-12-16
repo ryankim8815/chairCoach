1ab3fdd9bb915de0952db76031fe14cc387607ed
zzzzsb <ji12subin@gmail.com>
2022-12-16 11:28:11 +0900

    [Chore]: edit port number, url

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const backendPortNumber = "5003";
+const backendPortNumber = "5000";
 const serverUrl =
-  "http://" + window.location.hostname + ":" + backendPortNumber + "/";
+  "https://" + window.location.hostname + ":" + backendPortNumber + "/";
 
 async function updateToken() {
   if (localStorage.getItem("refreshToken")) {
